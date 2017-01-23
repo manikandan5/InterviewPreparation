edf9312ee3eb944ec7a3665e5a3407c4ec0f01f3
manikandan.m@live.co.uk <Hello2you!>
2017-01-22 21:17:43 -0500

    Fifth question

---
+++ CrackingTheCodingInterview/src/StringCompression.java
@@ -0,0 +1,56 @@
+/**
+ * Created by manikandan5 on 1/22/17.
+ */
+public class StringCompression
+{
+    private static String compressString(String s)
+    {
+        StringBuilder strBld = new StringBuilder();
+
+        int count, j;
+
+        for(int i=0; i < s.length() - 1; i++)
+        {
+            count = 1;
+            j = i + 1;
+            while(true)
+            {
+                if(s.charAt(i) != s.charAt(j))
+                {
+                    break;
+                }
+                count++;
+                if(j < s.length() - 1)
+                {
+                    j++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            i = j - 1;
+            strBld.append(s.charAt(i));
+            strBld.append(count);
+
+        }
+
+        if(s.charAt(s.length()-1) != s.charAt(s.length()-2))
+        {
+            strBld.append(s.charAt(s.length()-1));
+            strBld.append('1');
+        }
+
+        if(strBld.length() > s.length())
+        {
+            return s;
+        }
+        return String.valueOf(strBld);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(compressString("abbbbbb"));
+        System.out.println(compressString("aaaabbbb"));
+    }
+}
